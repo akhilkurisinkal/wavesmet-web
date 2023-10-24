b62e3b3f350bb41f9bfc2bdf0f451e91cd88d5a9
Akhil K Thomas <akhilkthomas99@gmail.com>
2023-10-24 07:40:51 -0400

    created textField

---
+++ src/components/textField/TextFieldWithEmailIcon.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./TextFieldWithEmailIcon.module.css";
+import ArrowBack from '@material-ui/icons/ArrowBackIos';
+
+const TextFieldWithEmailIcon=()=>{
+    return(
+        <div className={styles.textFieldWithIcon}>
+            <div>
+                
+            </div>
+            <div>
+                <input type="text" />
+            </div>
+        </div>
+    )
+}
+
+
+export default TextFieldWithEmailIcon;
--- src/pages/email-prompt/EmailPrompt.jsx
+++ src/pages/email-prompt/EmailPrompt.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import styles from "./EmailPrompt.module.css";
+import ArrowBack from '@material-ui/icons/ArrowBackIos';
+import TextFieldWithEmailIcon from "../../components/textField/TextFieldWithEmailIcon";
+
 
 const EmailPrompt=()=>{
     return(
         <div id="emailPrompt" className={styles.emailPrompt}>
             <div>
                 <div>
-                    <button className={styles.backBtn}>&nbsp;</button>
+                    <ArrowBack/>
                 </div>
                 <div>&nbsp;</div>
             </div>
             <div>
-                &nbsp;
+                <h3>Your Email Address</h3>
+                <p>Enter your email addrress to get started.</p>
+                <TextFieldWithEmailIcon/>
             </div>
         </div>
     )
