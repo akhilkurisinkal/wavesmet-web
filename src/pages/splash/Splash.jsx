434f7cafcbdb734bcbc24feb8dab0574f4d5b62f
Akhil K Thomas <akhilkthomas99@gmail.com>
2023-10-23 20:30:04 -0400

    created pages folder and Splash screen

---
+++ src/pages/splash/Splash.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "./Splash.module.css";
+import Icon from "../../components/icon/Icon";
+import { useNavigate } from "react-router-dom";
+
+
+const Splash=()=>{
+    
+    const navigate=useNavigate();
+
+    const gotoLogin=()=>{
+        navigate("/signup");
+    }
+
+    return(
+        <div id="splash" className={styles.splash}>
+            <div className={`${styles.containerFlexColumn} ${styles.verticalHorizondalCenter}`}>
+                <Icon/>
+            </div>
+           <div>
+                <button id="splash-btn-1" className={`${styles.containerFlexRow} ${styles.verticalHorizondalCenter}`} onClick={gotoLogin} >Continue with email</button>
+           </div>
+        </div>
+    )
+}
+
+
+export default Splash;
+
