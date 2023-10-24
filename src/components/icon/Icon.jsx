b7e459cdc51368d485bca482ee45cf85769cf453
Akhil K Thomas <akhilkthomas99@gmail.com>
2023-10-23 20:26:31 -0400

    created components folder and Icon component

---
+++ src/components/icon/Icon.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Icon.module.css";
+
+const Icon=()=>{
+    return(
+        <div id="icon" className={styles.icon}>
+            &nbsp;
+        </div>
+    )
+}
+
+export default Icon;
