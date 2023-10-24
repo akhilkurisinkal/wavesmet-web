1981bbecccc7ab1aaed1a520ce6fe45a7e630af9
Akhil K Thomas <akhilkthomas99@gmail.com>
2023-10-23 21:48:19 -0400

    added route to email prompt

--- src/RoutePath.jsx
+++ src/RoutePath.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Splash from "./pages/splash/Splash";
+import EmailPrompt from "./pages/email-prompt/EmailPrompt";
 
 
 const RoutePath=()=>{
     return(
         <Routes>
             <Route path="/" element={<Splash/>}/> 
+            <Route path="/emailPrompt" element={<EmailPrompt/>}/> 
         </Routes>
     )
 }
